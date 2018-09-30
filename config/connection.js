b17ca3cd35009c0ece3956d8e3856c6940829305
Angel Lugo <this@angellugo.com>
2018-09-30 13:16:55 -0700

    Adding JAWSDB for deployment to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,26 @@
 // Set up MySQL connection.
-const mysql = require("mysql");
+const mysql = require ('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
-
-const connectionLive = mysql.createConnection({
-  host: "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "o5z490og62i8mgc0",
-  password: "twy1t4q9neydei9d",
-  database: "ci8g41fzu5dy3mi3"
-});
+if (process.env.JAWSDB_URL) {
+  mysql.createConnection (process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection ({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db',
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect (function (err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error ('error connecting: ' + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log ('connected as id ' + connection.threadId);
 });
 
 // Export connection for our ORM to use.
